62fc8c5b36155c9292712929f8b06d2546b896a2
alejandro.ruiga <44670069+Algar487@users.noreply.github.com>
2022-05-28 09:48:01 +0200

    tarea dawdistancia22 actualizada

---
+++ Ruiz.php
@@ -0,0 +1,42 @@
+<?php
+/**
+  *  @author Alejandro Ruiz
+  *  @version 1.0
+  *  @internal Tarea 5 DAW
+  *  ARCHIVO MODIFICADO DESDE UBUNTU
+*/
+
+  /**
+    * Función suma de dos números
+    * @param int $num1
+    * @param int $num2
+    * @return float
+  */
+
+  function suma ($num1, $num2) {
+      $resultadosuma = $num1 + $num2;
+      return $resultadosuma;
+
+  }
+
+  
+  /**
+    * Función resta de dos números
+    * @param int $num1
+    * @param int $num2
+    * @return float
+  */
+
+  function resta ($num1, $num2) {
+    $resultadoresta = $num1 - $num2;
+    return $resultadoresta;
+
+}
+
+$suma = suma (24,42);
+$resta = resta (20,10);
+
+echo $suma;
+echo $resta;
+
+?>
